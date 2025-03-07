950b71359ad3d5db375a52f33d197d258ba85602
Overgrown <overgrownmc@gmail.com>
2025-03-07 15:32:22 -0500

    Initial commit

---
+++ src/main/java/dev/overgrown/thaumaturge/Thaumaturge.java
@@ -0,0 +1,17 @@
+package dev.overgrown.thaumaturge;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Thaumaturge implements ModInitializer {
+	public static final String MOD_ID = "thaumaturge";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/dev/overgrown/thaumaturge/ThaumaturgeClient.java
@@ -0,0 +1,10 @@
+package dev.overgrown.thaumaturge;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThaumaturgeClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
