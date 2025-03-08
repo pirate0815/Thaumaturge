f135dda339c52a18f53b90a08ab95821e4d54612
Overgrown <overgrownmc@gmail.com>
2025-03-08 14:52:42 -0500

    - Added Lesser Foci texture - Added ItemBuilder to help register and create items (thanks Robin)

--- src/main/java/dev/overgrown/thaumaturge/Thaumaturge.java
+++ src/main/java/dev/overgrown/thaumaturge/Thaumaturge.java
@@ -2,7 +2,7 @@ package dev.overgrown.thaumaturge;
 
 import dev.overgrown.thaumaturge.item.ModItems;
 import net.fabricmc.api.ModInitializer;
-
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,9 +10,12 @@ public class Thaumaturge implements ModInitializer {
 	public static final String MOD_ID = "thaumaturge";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+	public static Identifier identifier(String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+
 	@Override
 	public void onInitialize() {
-
-		ModItems.registerModItems();
+		ModItems.register();
 	}
 }
--- src/main/java/dev/overgrown/thaumaturge/item/ModItems.java
+++ src/main/java/dev/overgrown/thaumaturge/item/ModItems.java
@@ -1,27 +1,18 @@
 package dev.overgrown.thaumaturge.item;
 
 import dev.overgrown.thaumaturge.Thaumaturge;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import dev.overgrown.thaumaturge.utils.ItemBuilder;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
-import net.minecraft.util.Identifier;
 
 public class ModItems {
 
-    public static final Item LESSER_FOCI = registerItem(new Item(new Item.Settings().registryKey(RegistryKey.of(RegistryKeys.ITEM, Identifier.of(Thaumaturge.MOD_ID,"lesser_foci")))));
+    public static final Item LESSER_FOCI = ItemBuilder.create("lesser_foci")
+            .setItemGroup(ItemGroups.INGREDIENTS)
+            .withSettings(new Item.Settings().maxCount(1))
+            .buildAndRegister();
 
-    private static Item registerItem(Item item) {
-        return Registry.register(Registries.ITEM, Identifier.of(Thaumaturge.MOD_ID, "lesser_foci"), item);
-    }
-
-    public static void registerModItems() {
+    public static void register() {
         Thaumaturge.LOGGER.info("Registering Mod Items for " + Thaumaturge.MOD_ID);
-
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
-                .register((itemGroup) -> itemGroup.add(ModItems.LESSER_FOCI));
     }
 }
